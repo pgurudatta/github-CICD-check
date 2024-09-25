538673a4dfc4b1a10a01e1ef2f0ec2d26fb112a9
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-09-25 17:56:32 +0530

    Create injection.py

---
+++ injection.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+def get_user(username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE username = '" + username + "';"
+    cursor.execute(query)
+    user = cursor.fetchone()
+    conn.close()
+    return user
+
+username = input("Enter username: ")
+user = get_user(username)
+print(user)
